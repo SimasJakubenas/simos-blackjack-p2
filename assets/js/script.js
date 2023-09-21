292b7dabf01124f040919e2852714308dba11106
sjakubenas <sjakubenas@gmail.com>
2023-09-21 12:08:56 -0400

    Add deck of cards array and card images

---
+++ assets/js/script.js
@@ -0,0 +1,315 @@
+/* Deck of Cards array */
+let cards = [
+    {
+        name: "ace",
+        suit: "spades",
+        value: 11,
+        image: "ace-spades.png"
+    },
+    {
+        name: "king",
+        suit: "spades",
+        value: 10,
+        image: "king-spades.png"
+    },
+    {
+        name: "queen",
+        suit: "spades",
+        value: 10,
+        image: "queen-spades.png"
+    },
+    {
+        name: "jack",
+        suit: "spades",
+        value: 10,
+        image: "jack-spades.png"
+    },
+    {
+        name: "ten",
+        suit: "spades",
+        value: 10,
+        image: "10spades.png"
+    },
+    {
+        name: "nine",
+        suit: "spades",
+        value: 9,
+        image: "9spades.png"
+    },
+    {
+        name: "eight",
+        suit: "spades",
+        value: 8,
+        image: "8spades.png"
+    },
+    {
+        name: "seven",
+        suit: "spades",
+        value: 7,
+        image: "7spades.png"
+    },
+    {
+        name: "six",
+        suit: "spades",
+        value: 6,
+        image: "6spades.png"
+    },
+    {
+        name: "five",
+        suit: "spades",
+        value: 5,
+        image: "5spades.png"
+    },
+    {
+        name: "four",
+        suit: "spades",
+        value: 4,
+        image: "4spades.png"
+    },
+    {
+        name: "trey",
+        suit: "spades",
+        value: 11,
+        image: "3spades.png"
+    },
+    {
+        name: "deuce",
+        suit: "spades",
+        value: 11,
+        image: "2spades.png"
+    },
+    {
+        name: "ace",
+        suit: "clubs",
+        value: 11,
+        image: "ace-clubs.png"
+    },
+    {
+        name: "king",
+        suit: "clubs",
+        value: 10,
+        image: "king-clubs.png"
+    },
+    {
+        name: "queen",
+        suit: "clubs",
+        value: 10,
+        image: "queen-clubs.png"
+    },
+    {
+        name: "jack",
+        suit: "clubs",
+        value: 10,
+        image: "jack-clubs.png"
+    },
+    {
+        name: "ten",
+        suit: "clubs",
+        value: 10,
+        image: "10clubs.png"
+    },
+    {
+        name: "nine",
+        suit: "clubs",
+        value: 9,
+        image: "9clubs.png"
+    },
+    {
+        name: "eight",
+        suit: "clubs",
+        value: 8,
+        image: "8clubs.png"
+    },
+    {
+        name: "seven",
+        suit: "clubs",
+        value: 7,
+        image: "7clubs.png"
+    },
+    {
+        name: "six",
+        suit: "clubs",
+        value: 6,
+        image: "6clubs.png"
+    },
+    {
+        name: "five",
+        suit: "clubs",
+        value: 5,
+        image: "5clubs.png"
+    },
+    {
+        name: "four",
+        suit: "clubs",
+        value: 4,
+        image: "4clubs.png"
+    },
+    {
+        name: "trey",
+        suit: "clubs",
+        value: 11,
+        image: "3clubs.png"
+    },
+    {
+        name: "deuce",
+        suit: "clubs",
+        value: 11,
+        image: "2clubs.png"
+    },
+    {
+        name: "ace",
+        suit: "hearts",
+        value: 11,
+        image: "ace-hearts.png"
+    },
+    {
+        name: "king",
+        suit: "hearts",
+        value: 10,
+        image: "king-hearts.png"
+    },
+    {
+        name: "queen",
+        suit: "hearts",
+        value: 10,
+        image: "queen-hearts.png"
+    },
+    {
+        name: "jack",
+        suit: "hearts",
+        value: 10,
+        image: "jack-hearts.png"
+    },
+    {
+        name: "ten",
+        suit: "hearts",
+        value: 10,
+        image: "10hearts.png"
+    },
+    {
+        name: "nine",
+        suit: "hearts",
+        value: 9,
+        image: "9hearts.png"
+    },
+    {
+        name: "eight",
+        suit: "hearts",
+        value: 8,
+        image: "8hearts.png"
+    },
+    {
+        name: "seven",
+        suit: "hearts",
+        value: 7,
+        image: "7hearts.png"
+    },
+    {
+        name: "six",
+        suit: "hearts",
+        value: 6,
+        image: "6hearts.png"
+    },
+    {
+        name: "five",
+        suit: "hearts",
+        value: 5,
+        image: "5hearts.png"
+    },
+    {
+        name: "four",
+        suit: "hearts",
+        value: 4,
+        image: "4hearts.png"
+    },
+    {
+        name: "trey",
+        suit: "hearts",
+        value: 11,
+        image: "3hearts.png"
+    },
+    {
+        name: "deuce",
+        suit: "hearts",
+        value: 11,
+        image: "2hearts.png"
+    },
+    {
+        name: "ace",
+        suit: "diamonds",
+        value: 11,
+        image: "ace-diamonds.png"
+    },
+    {
+        name: "king",
+        suit: "diamonds",
+        value: 10,
+        image: "king-diamonds.png"
+    },
+    {
+        name: "queen",
+        suit: "diamonds",
+        value: 10,
+        image: "queen-diamonds.png"
+    },
+    {
+        name: "jack",
+        suit: "diamonds",
+        value: 10,
+        image: "jack-diamonds.png"
+    },
+    {
+        name: "ten",
+        suit: "diamonds",
+        value: 10,
+        image: "10diamonds.png"
+    },
+    {
+        name: "nine",
+        suit: "diamonds",
+        value: 9,
+        image: "9diamonds.png"
+    },
+    {
+        name: "eight",
+        suit: "diamonds",
+        value: 8,
+        image: "8diamonds.png"
+    },
+    {
+        name: "seven",
+        suit: "diamonds",
+        value: 7,
+        image: "7diamonds.png"
+    },
+    {
+        name: "six",
+        suit: "diamonds",
+        value: 6,
+        image: "6diamonds.png"
+    },
+    {
+        name: "five",
+        suit: "diamonds",
+        value: 5,
+        image: "5diamonds.png"
+    },
+    {
+        name: "four",
+        suit: "diamonds",
+        value: 4,
+        image: "4diamonds.png"
+    },
+    {
+        name: "trey",
+        suit: "diamonds",
+        value: 11,
+        image: "3diamonds.png"
+    },
+    {
+        name: "deuce",
+        suit: "diamonds",
+        value: 11,
+        image: "2diamonds.png"
+    },
+]
